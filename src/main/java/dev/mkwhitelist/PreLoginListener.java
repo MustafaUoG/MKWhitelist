fac9fe82a554508e66902eaabc621a02ca7c63b7
MustafaUoG <alkhazrm@uoguelph.ca>
2026-09-10 12:55:38 -0400

    PreLoginListener updated to enforce our whitelist rules
    Automatically kicks the player as a fail safe if any of the conditions aren't satisfied, like the bot not being in the server/guild, player not linked their account, and unlinked players receiving their code during join-kick

--- src/main/java/dev/mkwhitelist/PreLoginListener.java
+++ src/main/java/dev/mkwhitelist/PreLoginListener.java
@@ -20,7 +20,28 @@ public class PreLoginListener implements Listener {
 
         //checks for discord will go here, for now we will only log what we are seeing
 
+
+        if (!plugin.isDiscordConnected()){
+            event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, "Server setup incomplete. Please contact an admin.");
+            return;
+        }
+
+        String discordId = plugin.getDatabaseManager().getLinkedDiscordId(playerUUID.toString());
+
+        if (discordId == null){
+            String code = plugin.generateLinkCode(playerUUID.toString());
+            event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, "You need to link your Discord Account first!\n" + "Join our discord server and run /link" + code);
+            return;
+        }
+
+        if (!plugin.isMemberofGuild(discordId)) {
+            event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, "You must be a member of our Discord server to join!");
+            return;
+        }
+
+
         plugin.getLogger().info("Pre-Login check for UUID: " + playerUUID);
         plugin.getLogger().info("Server online-mode: " + plugin.isOnlineMode());
+        plugin.getLogger().info("Player " + playerUUID + "has passed the whitelist check.");
     }
 }
